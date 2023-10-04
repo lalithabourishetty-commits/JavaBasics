2c5074fd8460ff2aa5e0fe956b098c88881e324f
lalitha.bourishetty <lalitha.bourishetty@infosys.com>
2023-10-04 20:17:48 +0530

    Logic for basic pattern using java

--- SquarePattern.java
+++ SquarePattern.java
@@ -1,16 +1,41 @@
 public class SquarePattern {
-    public static void main(String args[]) {
-       // squarePattern(3);
-       // crossPattern(5);
-       //hallowSquare(5);
-       //hallowIncreasingTriangle(5);
-        hallowDecreasingTriangle(5);
+    public static void main(String[] args) {
+        // squarePattern(3);
+        // crossPattern(4);
+        //hallowSquare(6);
+        //hallowIncreasingTriangle(9);
+        //hallowDecreasingTriangle(5);
+        hillPattern(6);
+    }
+
+    private static void hillPattern(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n - row; col++) {
+                System.out.print("  ");
+            }
+            for (int col = 0; col < row; col++) {
+                if (row == n - 1 || col == 0)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            for (int col = 1; col < row; col++) {
+                if (row == n - 1 || col == row - 1)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
     }
 
     private static void hallowDecreasingTriangle(int number) {
-        for (int row =number-1; row >=0; row--) {
-            for (int colmn = 0; colmn <=row; colmn++) {
-                System.out.print("* ");
+        for (int row = 0; row < number; row++) {
+            for (int colmn = 0; colmn < number - row; colmn++) {
+                if (row == 0 || colmn == 0 || row + colmn == number - 1)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
             }
             System.out.println();
         }
@@ -18,8 +43,8 @@ public class SquarePattern {
 
     private static void hallowIncreasingTriangle(int n) {
         for (int row = 0; row < n; row++) {
-            for (int colmn = 0; colmn <=row; colmn++) {
-                if(colmn==0||row==n-1||row==colmn)
+            for (int colmn = 0; colmn <= row; colmn++) {
+                if (colmn == 0 || row == n - 1 || row == colmn)
                     System.out.print("* ");
                 else
                     System.out.print("  ");
@@ -31,7 +56,7 @@ public class SquarePattern {
     private static void crossPattern(int n) {
         for (int row = 0; row < n; row++) {
             for (int colmn = 0; colmn < n; colmn++) {
-                if ((row == colmn) || (row + colmn == n-1))
+                if ((row == colmn) || (row + colmn == n - 1))
                     System.out.print("* ");
                 else
                     System.out.print("  ");
@@ -43,7 +68,7 @@ public class SquarePattern {
     private static void hallowSquare(int n) {
         for (int row = 0; row < n; row++) {
             for (int colmn = 0; colmn < n; colmn++) {
-                if ((row == 0) || (row == n-1) ||(colmn == 0)||(colmn == n-1))
+                if ((row == 0) || (row == n - 1) || (colmn == 0) || (colmn == n - 1))
                     System.out.print("* ");
                 else
                     System.out.print("  ");
